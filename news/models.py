4d96bc2276c79ad8bea2a7f7b03b4ff894ada706
ViktorSanych <i_vitek@mail.ru>
2023-04-11 14:13:46 +0300

    Подключил метод censor в шаблоне default.html, использовал в заголовках и текстах.

--- news/models.py
+++ news/models.py
@@ -76,18 +76,6 @@ class Post(models.Model):
     def get_summary_comments_rating(self):
         return sum([comment.rate for comment in self.comment_set.all()])
 
-    def censor(self):
-        if not isinstance(self.text, str):
-            raise ValueError('Можно применить только к строке!')  # Если строка, то в ней уже ищем плохие слова
-        if 'редиск' or 'баран' in self.text:
-            self.text = self.text.replace('редиск', 'р******')
-            self.text = self.text.replace('баран', 'б****')
-            return f'{self.text}'
-
-    def preview(self):
-        self.text = Post.censor(self)
-        return f'{self.text[:20]}...'
-
 
 class PostCategory(models.Model):
     class Meta:
---
+++ news/templatetags/custom_filters.py
@@ -0,0 +1,15 @@
+from news.models import Post
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter(name='censor')
+def censor(text=Post.text):
+    if not isinstance(text, str):
+        raise ValueError('Можно применить только к строке!')  # Если строка, то в ней уже ищем плохие слова
+    if 'редиск' or 'баран' in text:
+        text = text.replace('редиск', 'р******')
+        text = text.replace('баран', 'б****')
+        return text
